efe2cd234e72d64f22b96070b4b5707179f8f1fd
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2020-01-23 21:01:06 -0600

    add nunit test

---
+++ Jenkins_UnitTetProjet/Jenkins_UnitTetProjet/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Jenkins_UnitTetProjet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Jenkins_UnitTetProjet")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("23238ec5-a5d3-4611-894e-a93130957bfa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Jenkins_UnitTetProjet/Jenkins_UnitTetProjet/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Jenkins_UnitTetProjet
+{
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            driver.Url = "https://www.google.com/";
+            driver.FindElement(By.Name("q")).SendKeys("Selenium");
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            driver.Url = "https://www.google.com/";
+        }
+    }
+}
